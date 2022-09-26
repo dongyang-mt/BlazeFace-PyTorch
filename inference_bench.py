2958a321a68785a77b44357a0212a4e0e3890805
dong.yang <dong.yang@mthreads.com>
2022-09-26 12:54:24 +0000

    add benchmark in Linux CPU

--- inference_bench.py
+++ inference_bench.py
@@ -7,8 +7,8 @@ print("PyTorch version:", torch.__version__)
 print("CUDA version:", torch.version.cuda)
 print("cuDNN version:", torch.backends.cudnn.version())
 
-gpu = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-gpu
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -48,12 +48,24 @@ def plot_detections(img, detections, with_keypoints=True):
 
     plt.show()
 
+def save_benchmark(timecost, modulename, torch_device="cpu"):
+    cost_np = np.array(timecost)
+    print("==>> cost_np.shape: ", cost_np.shape)
+    print("==>> type(cost_np): ", type(cost_np))
+    import csv
+    csvFile = open(torch_device + "_analyze.csv", 'w', newline='')
+    writer = csv.writer(csvFile)
+    writer.writerow([torch_device, "mean", "std", "min", "max", "count"])
+    for i, name in enumerate(modulename):
+        writer.writerow([name, cost_np[:,i].mean(), cost_np[:,i].std(), cost_np[:,i].min(), cost_np[:,i].max(), cost_np[:,i].shape[0]])
+    np.savetxt(torch_device + "_timecost.csv", cost_np, delimiter=",")
+
 from blazeface import BlazeFace
 
-front_net = BlazeFace().to(gpu)
+front_net = BlazeFace().to(device)
 front_net.load_weights("blazeface.pth")
 front_net.load_anchors("anchors.npy")
-back_net = BlazeFace(back_model=True).to(gpu)
+back_net = BlazeFace(back_model=True).to(device)
 back_net.load_weights("blazefaceback.pth")
 back_net.load_anchors("anchorsback.npy")
 
@@ -70,12 +82,15 @@ front_detections = front_net.predict_on_image(img)
 
 t2 = time.time()
 print("blazeface timecost:", (t2 - t1) * 1000.0, 'ms')
-
+timecost = []
 for i in range(100):
     t1 = time.time()
     front_detections = front_net.predict_on_image(img)
     t2 = time.time()
-    print("blazeface timecost:", (t2 - t1) * 1000.0, 'ms')
+    timecost.append([(t2 - t1) * 1000.0])
+    # print("blazeface timecost:", (t2 - t1) * 1000.0, 'ms')
+
+save_benchmark(timecost, ["BlazeFace"], "cpu")
 
 front_detections.shape
 
