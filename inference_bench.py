0406627b671719115ae08c4db2c9b67972344274
dong.yang <dong.yang@mthreads.com>
2022-09-26 12:27:57 +0000

    add inference_bench.py

---
+++ inference_bench.py
@@ -0,0 +1,84 @@
+import numpy as np
+import torch
+import cv2
+import time
+
+print("PyTorch version:", torch.__version__)
+print("CUDA version:", torch.version.cuda)
+print("cuDNN version:", torch.backends.cudnn.version())
+
+gpu = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+gpu
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+def plot_detections(img, detections, with_keypoints=True):
+    fig, ax = plt.subplots(1, figsize=(10, 10))
+    ax.grid(False)
+    ax.imshow(img)
+
+    if isinstance(detections, torch.Tensor):
+        detections = detections.cpu().numpy()
+
+    if detections.ndim == 1:
+        detections = np.expand_dims(detections, axis=0)
+
+    print("Found %d faces" % detections.shape[0])
+
+    for i in range(detections.shape[0]):
+        ymin = detections[i, 0] * img.shape[0]
+        xmin = detections[i, 1] * img.shape[1]
+        ymax = detections[i, 2] * img.shape[0]
+        xmax = detections[i, 3] * img.shape[1]
+
+        rect = patches.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                 linewidth=1, edgecolor="r", facecolor="none",
+                                 alpha=detections[i, 16])
+        ax.add_patch(rect)
+
+        if with_keypoints:
+            for k in range(6):
+                kp_x = detections[i, 4 + k*2    ] * img.shape[1]
+                kp_y = detections[i, 4 + k*2 + 1] * img.shape[0]
+                circle = patches.Circle((kp_x, kp_y), radius=0.5, linewidth=1,
+                                        edgecolor="lightskyblue", facecolor="none",
+                                        alpha=detections[i, 16])
+                ax.add_patch(circle)
+
+    plt.show()
+
+from blazeface import BlazeFace
+
+front_net = BlazeFace().to(gpu)
+front_net.load_weights("blazeface.pth")
+front_net.load_anchors("anchors.npy")
+back_net = BlazeFace(back_model=True).to(gpu)
+back_net.load_weights("blazefaceback.pth")
+back_net.load_anchors("anchorsback.npy")
+
+# Optionally change the thresholds:
+front_net.min_score_thresh = 0.75
+front_net.min_suppression_threshold = 0.3
+
+t1 = time.time()
+
+img = cv2.imread("1face.png")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+front_detections = front_net.predict_on_image(img)
+
+t2 = time.time()
+print("blazeface timecost:", (t2 - t1) * 1000.0, 'ms')
+
+for i in range(100):
+    t1 = time.time()
+    front_detections = front_net.predict_on_image(img)
+    t2 = time.time()
+    print("blazeface timecost:", (t2 - t1) * 1000.0, 'ms')
+
+front_detections.shape
+
+front_detections
+
+plot_detections(img, front_detections)
